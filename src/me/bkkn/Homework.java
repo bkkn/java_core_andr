73b161fbde7758c0829d9153842a76015abeb7da
bkkn <svbkkn@yandex.ru>
2021-11-19 00:13:32 +0300

    Lesson 1 homework completed

---
+++ src/me/bkkn/Homework.java
@@ -0,0 +1,19 @@
+package me.bkkn;
+
+import me.bkkn.lesson1.Task;
+
+public class Homework {
+    public static void main(String[] args) {
+        lesson1Hw();
+    }
+
+    private static void lesson1Hw() {
+        float ans1 = Task.method1(1, 2, 3, 4);
+        boolean ans2 = Task.method2(10, 3);
+        Task.method3(1);
+        boolean ans4 = Task.method4(1);
+        Task.method5("Name");
+        boolean ans6 = Task.method6(2021);
+
+    }
+}
