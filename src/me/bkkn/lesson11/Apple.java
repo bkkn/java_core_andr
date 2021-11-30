e40bd819c5baa10699896861255282ddbd5e0c96
bkkn <svbkkn@yandex.ru>
2021-11-30 15:49:50 +0300

    Lesson 10 homework completed

--- src/me/bkkn/lesson11/Apple.java
+++ src/me/bkkn/lesson11/Apple.java
@@ -1,8 +1,13 @@
 package me.bkkn.lesson11;
 
-public class Apple extends Fruit {
+public class Apple extends Fruit implements isApple {
     @Override
     float getWeight() {
         return 1.0f;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple";
     }
 }
--- src/me/bkkn/lesson11/Box.java
+++ src/me/bkkn/lesson11/Box.java
@@ -1,29 +1,89 @@
 package me.bkkn.lesson11;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class Box<F extends Fruit> {
-    private ArrayList<F> list = new ArrayList<>();
+    public Box(Class<F> type) {
+        this.list = new ArrayList<>();
+        this.type = type;
+    }
+
+    public static Box<Apple> newAppleBox(){
+        return new Box<Apple>(Apple.class);
+    }
+
+    public static Box<Orange> newOrangeBox(){
+        return new Box<Orange>(Orange.class);
+    }
+
+
+    public static <T extends isApple> boolean appleMethod(Box<? extends isApple> box) {
+        return true;
+    }
+
+    public static <T extends isOrange> boolean orangeMethod(Box<? extends isOrange> box) {
+        return true;
+    }
+
+    private Class<F> type = null;
+
+    public Box(F fruitType) {
+        this.list = new ArrayList<>();
+        this.list.add(fruitType);
+        this.type = (Class<F>) fruitType.getClass();
+    }
+
+    public Class<F> getMyType() {
+        return this.type;
+    }
+
+    private ArrayList<Fruit> list = new ArrayList<>();
     private static float epsilon = 1e-5f;
 
-    public Box(ArrayList<F> list) {
+    public Box(ArrayList<Fruit> list) {
         this.list = list;
     }
 
-    public Box(F[] array) {
-        for (F fruit : array)
-            this.list.add(fruit);
+//    public <FruitType extends Fruit> Box(F[] array) {
+//        object = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i].getClass().getName().equals(object.getClass().getName()))
+//                this.list.add(array[i]);
+//            else
+//                System.out.println("Class type mismatch; " + array[i] + " skipped");
+//        }
+//}
+
+//    public <FruitType extends Fruit> Box(FruitType fruit) {
+//        this.list.add(fruit);
+//        object = fruit;
+//    }
+
+    public Box(Class<F> type, int count) {
+        try {
+            for (int i = 0; i < count; i++)
+                list.add((type.getDeclaredConstructor().newInstance()));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
-
-    public Box(F fruit) {
-        this.list.add(fruit);
-    }
+//    public Box(Fruit[] array) {
+//        if (array.length != 0)
+//            this.type = (Class<F>) array[0].getClass();
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i].getClass().equals(this.type))
+//                list.add(array[i]);
+//            else
+//                System.out.println("not a " + array[0] + "; skipped" );
+//        }
+//    }
 
     public float getWeight() {
-       float sum = 0.f;
-        for (F fruit :
+        float sum = 0.f;
+        for (Fruit fruit :
                 list) {
             sum += fruit.getWeight();
         }
@@ -31,15 +91,27 @@ public class Box<F extends Fruit> {
     }
 
     public void add(F fruit) {
-        list.add(fruit);
+        list.add((Fruit) fruit);
     }
 
-    public void pourIn(Box other){
-        this.list.addAll(other.list);
+    public Box<F> pourIn(Box<F> other) {
+        Class<F> otherType = other.getMyType();
+        boolean sameType = otherType.getTypeName().equals(this.getMyType().getTypeName());
+        if (sameType)
+            this.list.addAll(other.list);
+        else
+            System.out.println("different type");
+        return this;
     }
 
-    public void pourOut(Box other){
-        other.list.addAll(this.list);
+    public Box<F> pourOut(Box<F> other) {
+        Class<F> otherType = other.getMyType();
+        boolean sameType = otherType.getTypeName().equals(this.getMyType().getTypeName());
+        if (sameType)
+            this.list.addAll(other.list);
+        else
+            System.out.println("different type");
+        return this;
     }
 
     public <Other extends Fruit> boolean compare(Box<Other> otherBox) {
@@ -53,5 +125,4 @@ public class Box<F extends Fruit> {
     public static <F1 extends Fruit, F2 extends Fruit> boolean compareEpsilon(Box<F1> box1, Box<F2> box2, float epsilon) {
         return Math.abs(box1.getWeight() - box2.getWeight()) < epsilon;
     }
-
 }
